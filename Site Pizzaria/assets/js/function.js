9d2b90d6d73878d29ecb9c300cc92f172fdc9570
Brunadisner <bruna.disner@gmail.com>
2021-04-11 23:10:03 -0300

    js

--- Site Pizzaria/assets/js/function.js
+++ Site Pizzaria/assets/js/function.js
@@ -7,3 +7,24 @@ function minChat(){
     texto = document.getElementById("texto");
     texto.innerHTML = 'Chat';
 }
+function maxChat(){
+    chat = document.getElementById("chat");
+    chat.style.height = '150px';
+    texto = document.getElementById("texto");
+    texto.innerHTML = 'Clique aqui para falar com os atendentes';
+}
+function showMenu(){
+    menu = document.getElementsByTagName("nav")[0];
+    if(menu.style.display == 'block')
+        menu.style.display = 'none';
+    else
+        menu.style.display = 'block';
+}
+{
+    document.body.onresize = function resize(){
+        window.innerWidth;
+        if(largura >= 700){
+            menu.style.display = 'block';
+        }   
+    }
+};
