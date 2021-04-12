681d0f4d785f3c203ac30c83c1832f8c43b56578
Brunadisner <bruna.disner@gmail.com>
2021-04-12 10:41:14 -0300

    js

--- Site Pizzaria/assets/js/function.js
+++ Site Pizzaria/assets/js/function.js
@@ -21,8 +21,9 @@ function showMenu(){
         menu.style.display = 'block';
 }
 
-document.body.onresize = function resize(){
-    window.innerWidth;
+document.body.onresize = function(){
+    largura = window.innerWidth;
+    menu = document.getElementsByTagName("nav")[0];
     if(largura >= 700){
          menu.style.display = 'block';
     }  
@@ -30,14 +31,57 @@ document.body.onresize = function resize(){
         menu.style.display = 'none';
     } 
 };
+var sizes = {
+    'p': 2,
+    'm': 3,
+    'g': 4,
+    'gg': 4,
+}
 
 function selectPizzaSize(){
-    currentSize = document.getElementById("tamanho").Value;
+    currentSize = document.getElementById("tamanho").value;
     if(currentSize == ""){
         document.getElementById("opcoes_pedido").style.display = "none";
     }
     else{
         document.getElementById("opcoes_pedido").style.display = "block";
     }
+    document.getElementById("limiteSabores").innerText = sizes[currentSize];
 
+    lista=document.querySelectorAll("input[type=checkbox]");
+    for (i = 0; i < lista.length; i++)
+        lista[i].checked = false;
+    document.getElementById("numSabores").innerText = 0;
+}
+
+function countSabores(){
+    total = 0;
+    lista=document.querySelectorAll("input[type=checkbox]");
+    for (i = 0; i < lista.length; i++)
+        if(lista[i].checked)
+            total++;
+    return total;
+}
+
+function updateCount(){
+    currentSize = document.getElementById("tamanho").value;
+    countSelected = countSabores();
+    document.getElementById("numSabores").innerText = countSelected;
+    if(countSelected > sizes[currentSize])
+        alert("Você ultrapassou o número de sabores permitido");
+}
+
+function addToCart(){
+    currentSize = document.getElementById("tamanho").value;
+    countSelected = countSabores();
+    if(countSelected > sizes[currentSize]){
+        alert("Você ultrapassou o número de sabores permitido");
+        return false;
+    }
+    else{
+        if(countSelected == 0){
+            alert("Selecione pelo menos 1 sabor")
+            return false;
+        }
+    }
 }
