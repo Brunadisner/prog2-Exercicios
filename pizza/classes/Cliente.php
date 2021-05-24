81dc3596f36af66148c019d37f48b3c375a4c79e
Brunadisner <bruna.disner@gmail.com>
2021-05-24 10:38:07 -0300

    arrumando

---
+++ pizza/classes/Cliente.php
@@ -0,0 +1,24 @@
+<?php
+
+class cliente{
+
+    private $codigo;
+    private $nome;
+    private $email;
+    private $telefone;
+    private $endereco;
+    private $bairro;
+    private $senha;
+    private $senha2;
+    private $comoConheceu;
+    private $promoPizza;
+    
+}
+
+
+
+
+
+
+
+?>
